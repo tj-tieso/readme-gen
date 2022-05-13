c8c33b02df3e5be2403370cccb78f4f67739e06a
tj-tieso <tj.tieso@gmail.com>
2022-05-13 13:43:29 -0500

    gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +8,9 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  console.log("hello there");
+}
 
 // Function call to initialize app
 init();
