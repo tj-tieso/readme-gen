55c2709f126fb6c4b0fef69966f2ac00871a6d99
tj-tieso <tj.tieso@gmail.com>
2022-05-16 14:08:51 -0500

    questions added, README generating

--- index.js
+++ index.js
@@ -1,32 +1,135 @@
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions (with email)
+
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-
-const projectDataArgs = process.argv.slice(2, process.argv.length);
-const [title, github] = projectDataArgs;
-
-//test function
-const generatePage = (title, github) => {
-  return `
-  # ${title}
-  
-  ### My Github: [${github}](https://github.com/)https://github.com/${github})
-  `;
-};
-
-console.log(title, github);
-console.log(generatePage(title, github));
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // // TODO: Create an array of questions for user input
-// const questions = [];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub Username?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub Username");
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of the project?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the title of the project");
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a description of the project",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please provide a description of the project!");
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "Please provide installation instructions for the project",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log(
+            "Please provide installation instructions for the project!"
+          );
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please describe the intended usage for the project",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please describe the intended usage for the project!");
+        }
+      },
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Which license applies to the project?",
+      choices: ["MIT", "Harvard", "Yale"],
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: "Please add any contributing sources.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please provide your email address.",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please provide your email address!");
+        }
+      },
+    },
+  ]);
+};
 
 // // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+const writeFile = (fileContent) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/README.md", fileContent, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: "File Created!",
+      });
+    });
+  });
+};
 
 // // TODO: Create a function to initialize app
-// function init() {
-//   console.log("this is working");
-// }
+promptUser()
+  .then((data) => {
+    return generateMarkdown(data);
+  })
+  .then((pageMd) => {
+    return writeFile(pageMd);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // // Function call to initialize app
 // init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,19 +4,40 @@ const fs = require("fs");
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = (data) => {
+  return `
+  # ${data.title}
+  
+  ### Project Description
+  ${data.description}
 
-`;
-}
+  ## Table of Contents
+  -[Installation](${data.install})
+  -[Usage](${data.usage})
+  -[Contributors](${data.contribute})
+
+
+  ### Installation Instructions
+  ${data.install}
+
+
+  ### Intended Usage
+  ${data.usage}
+
+
+  #### License
+  ${data.license}
+
+
+  #### Contributors
+  ${data.contribute}
+
+  ## My Github: [${data.github}](https://github.com/${data.github})
+  #### Questions? Contact me at:
+  ${data.email}
+
+  `;
+};
 
 module.exports = generateMarkdown;
